21fd97ad9e449c703c05d623c2588d6e05424437
Fabrício <fabriciolak12@gmail.com>
2022-12-22 09:39:49 -0300

    feat: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,13 @@ export function App() {
         <main>
           <div className="movies-list">
             {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+              <MovieCard 
+                key ={movie.imdbID} 
+                title={movie.Title} 
+                poster={movie.Poster} 
+                runtime={movie.Runtime} 
+                rating={movie.Ratings[0].Value} 
+              />
             ))}
           </div>
         </main>
